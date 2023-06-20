a9799073b464df6a1c2bd56e0245d5e6194ada5d
sqjian <shengqi.jian@qq.com>
2023-06-20 03:42:39 +0000

    add some python3.11  soft links for comfortable usage

--- dockerfiles/os/ubuntu/scripts/tools_from_sources.sh
+++ dockerfiles/os/ubuntu/scripts/tools_from_sources.sh
@@ -84,7 +84,7 @@ function dev_tools_from_source() {
         which gcc
     }
 
-    function install_cmake_from_source() {
+    install_cmake_from_source() {
         apt-get update -y
         apt-get install -y wget gzip curl git openssl libssl-dev coreutils
 
@@ -121,72 +121,129 @@ function dev_tools_from_source() {
     }
 
     function install_python_from_source() {
-        apt-get update -y
-        apt-get install -y \
-            make \
-            libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl libncurses5-dev libncursesw5-dev xz-utils tk-dev liblzma-dev tk-dev
-        apt-get install -y python python-pip python3 python3-pip coreutils
+        build_python_from_source() {
+            apt-get update -y
+            apt-get install -y \
+                make \
+                libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl libncurses5-dev libncursesw5-dev xz-utils tk-dev liblzma-dev tk-dev
+            apt-get install -y python python-pip python3 python3-pip coreutils
 
-        check_command g++
-        check_command gcc
+            check_command g++
+            check_command gcc
 
-        local Directory=$(mktemp -d /tmp/python.XXXXXX)
+            local Directory=$(mktemp -d /tmp/python.XXXXXX)
 
-        if [ -d "$Directory" ]; then
-            echo "Directory $Directory exists. Removing and recreating it..."
-            rm -rf "$Directory"
-            mkdir "$Directory"
-        else
-            echo "Directory $Directory does not exist. Creating it..."
-            mkdir -p "$Directory"
-        fi
+            if [ -d "$Directory" ]; then
+                echo "Directory $Directory exists. Removing and recreating it..."
+                rm -rf "$Directory"
+                mkdir "$Directory"
+            else
+                echo "Directory $Directory does not exist. Creating it..."
+                mkdir -p "$Directory"
+            fi
 
-        curl -so- https://www.python.org/ftp/python/3.11.4/Python-3.11.4.tgz | tar --strip-components 1 -C "${Directory}" -xzf -
-        pushd "${Directory}"
-        ./configure \
-            --enable-optimizations \
-            --with-lto \
-            --with-computed-gotos \
-            --with-system-ffi \
-            --with-ensurepip=install \
-            --prefix=/opt/python
+            curl -so- https://www.python.org/ftp/python/3.11.4/Python-3.11.4.tgz | tar --strip-components 1 -C "${Directory}" -xzf -
+            pushd "${Directory}"
+            ./configure \
+                --enable-optimizations \
+                --with-lto \
+                --with-computed-gotos \
+                --with-system-ffi \
+                --with-ensurepip=install \
+                --prefix=/opt/python
 
-        make -j "$(nproc)"
-        make altinstall
-        popd
-        rm -rf "${Directory}"
+            make -j "$(nproc)"
+            make altinstall
+            popd
+            rm -rf "${Directory}"
 
-        py2="$(readlink -f "$(which python)")"
-        pip2="$(readlink -f "$(which pip)")"
+            /opt/python/bin/python3.11 -m pip install --upgrade pip setuptools wheel
+        }
 
-        py3="$(readlink -f "$(which python3)")"
-        pip3="$(readlink -f "$(which pip3)")"
+        update_alternatives() {
+            rm -rf /usr/local/bin/{python,python2,python3}
+            rm -rf /usr/local/bin/{pip,pip2,pip3}
+            rm -rf /usr/local/bin/{pydoc,pydoc2,pydoc3}
+            rm -rf /usr/local/bin/{python-config,python2-config,python3-config}
 
-        py_custom="/opt/python/bin/python3.11"
-        pip_custom="/opt/python/bin/pip3.11"
+            python2="$(readlink -f "$(which python2)")"
+            pip2="$(readlink -f "$(which pip)")"
+            pydoc2="$(readlink -f "$(which pydoc2.7)")"
+            python2_config="$(readlink -f "$(which python2-config)")"
 
-        update-alternatives --remove-all pip2 || true
-        update-alternatives --remove-all python2 || true
-        update-alternatives --install /usr/local/bin/python2 python2 "${py2}" 1 --slave /usr/local/bin/pip2 pip2 "${pip2}" || (echo "set python2 alternatives failed" && exit 1)
-        update-alternatives --auto python2
-        update-alternatives --display python2
+            python3="$(readlink -f "$(which python3)")"
+            pip3="$(readlink -f "$(which pip3)")"
+            pydoc3="$(readlink -f "$(which pydoc3)")"
+            python3_config="$(readlink -f "$(which python3-config)")"
 
-        update-alternatives --remove-all pip3 || true
-        update-alternatives --remove-all python3 || true
-        update-alternatives --install /usr/local/bin/python3 python3 "${py3}" 1 --slave /usr/local/bin/pip3 pip3 "${pip3}" || (echo "set python3 alternatives failed" && exit 1)
-        update-alternatives --install /usr/local/bin/python3 python3 "${py_custom}" 2 --slave /usr/local/bin/pip3 pip3 "${pip_custom}" || (echo "set python3 alternatives failed" && exit 1)
-        update-alternatives --auto python3
-        update-alternatives --display python3
+            py_custom="/opt/python/bin/python3.11"
+            pip_custom="/opt/python/bin/pip3.11"
+            pydoc_custom="/opt/python/bin/pydoc3.11"
+            python_config_custom="/opt/python/bin/python3.11-config"
 
-        update-alternatives --remove-all pip || true
-        update-alternatives --remove-all python || true
-        update-alternatives --install /usr/local/bin/python python /usr/local/bin/python2 1 --slave /usr/local/bin/pip pip /usr/local/bin/pip2 || (echo "set python alternatives failed" && exit 1)
-        update-alternatives --install /usr/local/bin/python python /usr/local/bin/python3 2 --slave /usr/local/bin/pip pip /usr/local/bin/pip3 || (echo "set python alternatives failed" && exit 1)
-        update-alternatives --auto python
-        update-alternatives --display python
+            # -----------------------------------------------------------
 
-        python --version
-        which python
+            update-alternatives --remove-all pip2 || true
+            update-alternatives --remove-all python2 || true
+            update-alternatives --remove-all pydoc2 || true
+            update-alternatives --remove-all python2-config || true
+
+            update-alternatives --install /usr/local/bin/python2 python2 "${python2}" 1 \
+                --slave /usr/local/bin/pip2 pip2 "${pip2}" \
+                --slave /usr/local/bin/pydoc2 pydoc2 "${pydoc2}" \
+                --slave /usr/local/bin/python2-config python2-config "${python2_config}" || (echo "set python2 alternatives failed" && exit 1)
+
+            update-alternatives --auto python2
+            update-alternatives --display python2
+
+            # -----------------------------------------------------------
+
+            update-alternatives --remove-all pip3 || true
+            update-alternatives --remove-all python3 || true
+            update-alternatives --remove-all pydoc3 || true
+            update-alternatives --remove-all python3-config || true
+
+            update-alternatives --install /usr/local/bin/python3 python3 "${python3}" 1 \
+                --slave /usr/local/bin/pip3 pip3 "${pip3}" \
+                --slave /usr/local/bin/pydoc3 pydoc3 "${pydoc3}" \
+                --slave /usr/local/bin/python3-config python3-config "${python3_config}" ||
+                (echo "set python3 alternatives failed" && exit 1)
+
+            update-alternatives --install /usr/local/bin/python3 python3 "${py_custom}" 2 \
+                --slave /usr/local/bin/pip3 pip3 "${pip_custom}" \
+                --slave /usr/local/bin/pydoc3 pydoc3 "${pydoc_custom}" \
+                --slave /usr/local/bin/python3-config python3-config "${python_config_custom}" || (echo "set python3 alternatives failed" && exit 1)
+
+            update-alternatives --auto python3
+            update-alternatives --display python3
+
+            # -----------------------------------------------------------
+
+            update-alternatives --remove-all pip || true
+            update-alternatives --remove-all python || true
+            update-alternatives --remove-all pydoc || true
+            update-alternatives --remove-all python-config || true
+
+            update-alternatives --install /usr/local/bin/python python /usr/local/bin/python2 1 \
+                --slave /usr/local/bin/pip pip /usr/local/bin/pip2 \
+                --slave /usr/local/bin/pydoc pydoc /usr/local/bin/pydoc2 \
+                --slave /usr/local/bin/python-config python-config /usr/local/bin/python2-config || (echo "set python alternatives failed" && exit 1)
+
+            update-alternatives --install /usr/local/bin/python python /usr/local/bin/python3 2 \
+                --slave /usr/local/bin/pip pip /usr/local/bin/pip3 \
+                --slave /usr/local/bin/pydoc pydoc /usr/local/bin/pydoc3 \
+                --slave /usr/local/bin/python-config python-config /usr/local/bin/python3-config || (echo "set python alternatives failed" && exit 1)
+
+            update-alternatives --auto python
+            update-alternatives --display python
+
+            python --version
+            which python
+        }
+
+        build_python_from_source
+        update_alternatives
+
     }
 
     install_gdb_from_source() {
@@ -224,7 +281,7 @@ function dev_tools_from_source() {
 }
 
 function useful_tools_from_source() {
-    function install_vim_from_source() {
+    install_vim_from_source() {
         apt-get update -y
         apt-get install -y curl git openssl libssl-dev coreutils vim
         apt-get install -y libncurses5-dev libperl-dev python-dev ruby-dev mercurial checkinstall lua5.2 liblua5.2-dev
@@ -292,7 +349,7 @@ EOF
         vim --version
         which vim
     }
-    function install_curl_from_source() {
+    install_curl_from_source() {
         apt-get update -y
         apt-get install -y curl git openssl libssl-dev coreutils libtool libtool-bin
 
