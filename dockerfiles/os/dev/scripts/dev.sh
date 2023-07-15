09149556335c57538be5fe699f314952a106d75a
sqjian <shengqi.jian@qq.com>
2023-07-15 16:32:07 +0800

    update dev.sh

--- dockerfiles/os/dev/scripts/dev.sh
+++ dockerfiles/os/dev/scripts/dev.sh
@@ -3,3 +3,15 @@
 set -euo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
+
+function install_rust_tools() {
+    apt-get update -y
+    apt-get install -y libjemalloc-dev curl make g++ gcc
+
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    source "$HOME/.cargo/env"
+    cargo install hyperfine # A command-line benchmarking tool
+    cargo install ripgrep   # A modern grep command alternative
+}
+
+install_rust_tools
