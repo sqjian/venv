ed53b2aca3d76667b3b2b303a1d86fd9b9e2c0e9
sqjian <shengqi.jian@qq.com>
2023-07-23 16:37:35 +0800

    update upx

--- dockerfiles/os/ubuntu/scripts/tools_from_precompiled.sh
+++ dockerfiles/os/ubuntu/scripts/tools_from_precompiled.sh
@@ -310,7 +310,39 @@ function install_rust_tools() {
 
     install_hyperfine
     install_ripgrep
+}
 
+function install_upx() {
+    _install_upx() {
+        apt-get update -y
+        apt-get install -y curl xz-utils
+
+        local Directory=$(mktemp -d /tmp/upx.XXXXXX)
+
+        pushd "${Directory}"
+        curl -o upx.tar.xz -L 'https://github.com/upx/upx/releases/download/v4.0.2/upx-4.0.2-amd64_linux.tar.xz'
+        mkdir -p /usr/local/upx
+        tar -xJf upx.tar.xz --strip-components=1 -C /usr/local/upx
+        popd
+
+        rm -rf "${Directory}"
+    }
+
+    _update_alternatives() {
+
+        rm -rf /usr/local/bin/{python,pip,pydoc,python-config} || true
+
+        update-alternatives --remove-all upx || true
+        update-alternatives --install /usr/local/bin/upx upx "/usr/local/upx/upx" 1 || (echo "set python alternatives failed" && exit 1)
+        update-alternatives --auto upx
+        update-alternatives --display upx
+
+        upx --version
+        which upx
+    }
+
+    _install_upx
+    _update_alternatives
 }
 
 function main() {
@@ -323,6 +355,7 @@ function main() {
     install_go
     install_vim
     install_rust_tools
+    install_upx
 }
 
 main
