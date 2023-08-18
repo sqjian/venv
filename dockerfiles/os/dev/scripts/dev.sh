f3f1740107d19b5bfc29267d230b6ae9474144b3
sqjian <shengqi.jian@qq.com>
2023-08-18 17:26:25 +0800

    update

--- dockerfiles/os/dev/scripts/dev.sh
+++ dockerfiles/os/dev/scripts/dev.sh
@@ -4,26 +4,28 @@ set -euo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 
-function install_python() {
-    _install_conda() {
+function install_pkg() {
+    _install_step1() {
         apt-get update -y
         apt-get install -y wget
 
-        local Directory=$(mktemp -d /tmp/conda.XXXXXX)
+        local Directory=$(mktemp -d /tmp/pkg.step1.XXXXXX)
         pushd "${Directory}"
-        wget -O 'Anaconda-Linux-x86_64.sh' https://repo.anaconda.com/archive/Anaconda3-2023.07-2-Linux-x86_64.sh
-        bash Anaconda-Linux-x86_64.sh -b -p /usr/local/conda
+        echo "pkg.step1."
         popd
         rm -rf "${Directory}"
     }
 
-    _install_python() {
-        source /usr/local/conda/bin/activate
-        conda create -y -n python python=3.11.4
+    _install_step2() {
+        local Directory=$(mktemp -d /tmp/pkg.step2.XXXXXX)
+        pushd "${Directory}"
+        echo "pkg.stdp2."
+        popd
+        rm -rf "${Directory}"
     }
 
-    _install_conda
-    _install_python
+    _install_step1
+    _install_step2
 }
 
-install_python
+install_pkg
