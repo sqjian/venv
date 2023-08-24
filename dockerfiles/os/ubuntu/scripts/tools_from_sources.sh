c013d1590c63a28c3c695b4cf88281336152add7
sqjian <shengqi.jian@qq.com>
2023-08-24 09:01:49 +0800

    update conda config

--- dockerfiles/os/ubuntu/scripts/tools_from_sources.sh
+++ dockerfiles/os/ubuntu/scripts/tools_from_sources.sh
@@ -82,6 +82,8 @@ function install_python() {
         pushd "${Directory}"
         wget -O 'Miniconda3-latest-Linux-x86_64.sh' https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
         bash Miniconda3-latest-Linux-x86_64.sh -b -p /usr/local/conda
+        /usr/local/conda/bin/conda init --all
+        /usr/local/conda/bin/conda config --set auto_activate_base false
         popd
         rm -rf "${Directory}"
     }
