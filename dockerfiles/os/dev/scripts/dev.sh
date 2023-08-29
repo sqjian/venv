66cb976794e187bee36804160aea4f48cc0aa42a
sqjian <shengqi.jian@qq.com>
2023-08-29 09:52:45 +0800

    update

--- dockerfiles/os/dev/scripts/dev.sh
+++ dockerfiles/os/dev/scripts/dev.sh
@@ -24,3 +24,24 @@ function install_pkg() {
     _install_step1
     _install_step2
 }
+
+function install_git() {
+    apt-get update -y
+    apt-get install -y \
+        software-properties-common \
+        apt-transport-https \
+        ca-certificates \
+        gnupg-agent \
+        curl
+
+    add-apt-repository -y ppa:git-core/ppa
+    curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+    apt-get update -y
+    apt-get install -y git git-lfs
+}
+
+function main() {
+    install_git
+}
+
+main
