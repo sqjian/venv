47e3898c209ac7f053853bb00ba35f374beea495
sqjian <shengqi.jian@qq.com>
2024-05-08 14:34:24 +0800

    update dev.yaml

--- dockerfiles/os/dev/scripts/dev.sh
+++ dockerfiles/os/dev/scripts/dev.sh
@@ -24,3 +24,80 @@ function install_pkg() {
     _install_step1
     _install_step2
 }
+
+function install_tmux() {
+    _install_tmux_bin() {
+        echo "installing tmux..."
+        apt-get update -y
+        apt-get install -y tmux
+    }
+
+    _clean_old_tmux_cfg() {
+        local _old_tmux_cfg_dir=$1
+        rm -rf "${_old_tmux_cfg_dir}"/.tmux*
+    }
+
+    _install_gpakosz_tmux_config() {
+        local _gpakosz_tmux_config_dir=$1
+        git clone --depth=1 https://github.com/gpakosz/.tmux.git "${_gpakosz_tmux_config_dir}"/.tmux
+        ln -s "${_gpakosz_tmux_config_dir}/.tmux/.tmux.conf" "${_gpakosz_tmux_config_dir}/.tmux.conf"
+        cp "${_gpakosz_tmux_config_dir}"/.tmux/.tmux.conf.local "${_gpakosz_tmux_config_dir}"/.tmux.conf.local
+    }
+
+    _install_custom_tmux_config() {
+
+        local _custom_tmux_config_dir=$1
+
+        sed -i '/set -g prefix2 C-a/d' "${_custom_tmux_config_dir}"/.tmux.conf
+        sed -i '/bind C-a send-prefix -2/d' "${_custom_tmux_config_dir}"/.tmux.conf
+
+        tee -a "${_custom_tmux_config_dir}"/.tmux.conf.local >/dev/null <<'EOF'
+# 基础设置
+set-option -g default-command "fish"
+set-window-option -g clock-mode-style 24 # 24小时显示方式
+
+# 绑定hjkl键为面板切换的上下左右键
+bind -r k select-pane -U # 绑定k为↑,选择上面板
+bind -r j select-pane -D # 绑定j为↓,选择下面板
+bind -r h select-pane -L # 绑定h为←,选择左面板
+bind -r l select-pane -R # 绑定l为→,选择右面板
+
+# 绑定Ctrl+hjkl键为面板上下左右调整边缘的快捷指令
+bind -r ^k resizep -U 5 # 绑定Ctrl+k为往↑调整面板边缘5个单元格
+bind -r ^j resizep -D 5 # 绑定Ctrl+j为往↓调整面板边缘5个单元格
+bind -r ^h resizep -L 5 # 绑定Ctrl+h为往←调整面板边缘5个单元格
+bind -r ^l resizep -R 5 # 绑定Ctrl+l为往→调整面板边缘5个单元格
+
+# 交换面板
+bind ^u swapp -U # 与上面板交换
+bind ^d swapp -D # 与下面板交换
+
+# 切换窗口
+bind -r C-p previous-window # select previous window
+bind -r C-n next-window     # select next window
+EOF
+    }
+
+    local _tmux_root_dir="/root"
+
+    _install_tmux_bin
+    _clean_old_tmux_cfg ${_tmux_root_dir}
+    _install_gpakosz_tmux_config ${_tmux_root_dir}
+    _install_custom_tmux_config ${_tmux_root_dir}
+}
+
+function install_git() {
+    apt-get update -y
+    apt-get install -y \
+        software-properties-common \
+        apt-transport-https \
+        ca-certificates \
+        gnupg-agent
+
+    add-apt-repository -y ppa:git-core/ppa
+    apt-get update -y
+    apt-get install -y git
+}
+
+install_git
+install_tmux
