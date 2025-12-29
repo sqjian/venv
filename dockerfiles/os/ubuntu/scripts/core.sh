2201b6334718f704c322419b16dbed2ab9cc5cdf
sqjian <shengqi.jian@qq.com>
2025-12-29 15:28:40 +0000

    remove unused packages

--- dockerfiles/os/ubuntu/scripts/core.sh
+++ dockerfiles/os/ubuntu/scripts/core.sh
@@ -4,83 +4,6 @@ set -exo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 
-function install_apt_fast() {
-    echo "Installing apt-fast..."
-    apt-get update -y
-    apt-get install -y software-properties-common aria2
-
-    add-apt-repository -y ppa:apt-fast/stable
-    apt-get update -y
-    apt-get install -y apt-fast
-
-    echo debconf apt-fast/maxdownloads string 16 | debconf-set-selections
-    echo debconf apt-fast/dlflag boolean true | debconf-set-selections
-    echo debconf apt-fast/aptmanager string apt-get | debconf-set-selections
-}
-function install_tools() {
-    apt-get update -y
-    apt-get install -y \
-        binutils \
-        inetutils-ping \
-        iproute2 \
-        gawk \
-        wget \
-        curl \
-        jq \
-        rsync \
-        dos2unix \
-        tree \
-        pkg-config \
-        makeself \
-        socat \
-        zip unzip \
-        sshpass \
-        texinfo \
-        lrzsz \
-        aria2 \
-        p7zip-full p7zip-rar \
-        ffmpeg \
-        sqlite3 \
-        psmisc \
-        file \
-        procps \
-        telnet
-
-    apt-get install -y \
-        build-essential \
-        checkinstall \
-        gdb \
-        cmake
-
-    apt-get install -y \
-        libssl-dev \
-        libsqlite3-dev \
-        libgdbm-dev \
-        libbz2-dev \
-        libffi-dev \
-        libxml2-dev \
-        liblzma-dev \
-        libgoogle-perftools-dev \
-        nghttp2 \
-        libnghttp2-dev
-
-    apt-get install -y \
-        libx11-dev \
-        libxext-dev \
-        libxtst-dev \
-        libxrender-dev \
-        libxmu-dev \
-        libxmuu-dev \
-        libc6-dev \
-        libxcb1-dev \
-        libaio-dev
-
-    apt-get install -y \
-        vim \
-        tmux
-
-}
-
 function install_locales() {
     apt-get update -y
     apt-get install -y locales
@@ -92,30 +15,17 @@ export LANG=zh_CN.utf-8
 EOF
 }
 
-function install_extra_tools() {
+function install_fish() {
     apt-get update -y
     apt-get install -y \
-        libsndfile1 \
-        libsndfile1-dev \
-        libflac-dev
+        software-properties-common \
+        apt-transport-https \
+        ca-certificates \
+        gnupg-agent
 
-    apt-get install -y \
-        libjpeg-turbo8-dev \
-        zlib1g-dev \
-        libfreetype6-dev \
-        liblcms2-dev \
-        libopenjp2-7-dev \
-        libtiff5-dev \
-        libharfbuzz-dev \
-        libfribidi-dev
-
-    apt-get install -y \
-        tcl \
-        tcl-dev \
-        tk-dev \
-        dejagnu \
-        libncursesw5-dev \
-        libmpich-dev
+    add-apt-repository -y ppa:fish-shell/release-3
+    apt-get update -y
+    apt-get install -y fish
 }
 
 function install_git() {
@@ -133,39 +43,40 @@ function install_git() {
     apt-get update -y
     apt-get install -y git git-lfs
 }
-function install_shell() {
-    function install_zsh() {
-        apt-get update -y
-        apt-get install -y zsh wget git sed
 
-        local Directory
-        Directory=$(mktemp -d /tmp/zsh.XXXXXX)
-
-        pushd "${Directory}" || exit
-        rm -rf /root/.oh-my-zsh /root/.zshrc
-        git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git /root/.oh-my-zsh
-        cp /root/.oh-my-zsh/templates/zshrc.zsh-template /root/.zshrc
-        sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="ys"/' /root/.zshrc
-        popd || exit
-        rm -rf "${Directory}"
-    }
-
-    function install_fish() {
-        apt-get update -y
-        apt-get install -y \
-            software-properties-common \
-            apt-transport-https \
-            ca-certificates \
-            gnupg-agent
-
-        add-apt-repository -y ppa:fish-shell/release-3
-        apt-get update -y
-        apt-get install -y fish
-    }
-
-    install_zsh
-    install_fish
+function install_tools() {
+    apt-get update -y
+    apt-get install -y \
+        binutils \
+        inetutils-ping \
+        iproute2 \
+        gawk \
+        wget \
+        curl \
+        jq \
+        rsync \
+        dos2unix \
+        tree \
+        pkg-config \
+        socat \
+        zip unzip \
+        sshpass \
+        texinfo \
+        lrzsz \
+        aria2 \
+        p7zip-full p7zip-rar \
+        psmisc \
+        file \
+        procps \
+        telnet \
+        vim \
+        tmux \
+        build-essential \
+        checkinstall \
+        gdb \
+        cmake
 }
+
 function install_docker_cli() {
     apt-get update -y
     apt-get install -y \
@@ -181,13 +92,27 @@ function install_docker_cli() {
     apt-get update -y
     apt-get install -y docker-ce-cli
 }
+
+function install_apt_fast() {
+    apt-get update -y
+    apt-get install -y software-properties-common aria2
+
+    add-apt-repository -y ppa:apt-fast/stable
+    apt-get update -y
+    apt-get install -y apt-fast
+
+    echo debconf apt-fast/maxdownloads string 16 | debconf-set-selections
+    echo debconf apt-fast/dlflag boolean true | debconf-set-selections
+    echo debconf apt-fast/aptmanager string apt-get | debconf-set-selections
+}
+
 function main() {
     install_locales
-    install_shell
+    install_fish
     install_git
     install_tools
-    install_extra_tools
     install_docker_cli
+    install_apt_fast
 }
 
 main
