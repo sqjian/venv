5347317457a9eb488bb1d8b531c073feac77fd23
root <root@dx-a800-10.246.52.6.aipaas.cn>
2025-12-30 01:21:29 +0000

    optimize brew install script

--- dockerfiles/os/ubuntu/scripts/extra.sh
+++ dockerfiles/os/ubuntu/scripts/extra.sh
@@ -4,6 +4,9 @@ set -exo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 export NONINTERACTIVE=1
+export HOMEBREW_FORCE_VENDOR_RUBY=1      # 强制使用自带 Portable Ruby
+export HOMEBREW_NO_ANALYTICS=1           # 禁用分析（提前设置）
+export HOMEBREW_NO_AUTO_UPDATE=1         # 禁用自动更新（加速安装）
 
 function install_brew() {
     apt-get update -y
