a861c580dd3a5b45985ee33aec8b614346308dbf
sqjian <shengqi.jian@gmail.com>
2026-02-06 02:09:31 +0000

    optimize testcase build scripts

--- dockerfiles/os/testcase/scripts/testcase.sh
+++ dockerfiles/os/testcase/scripts/testcase.sh
@@ -5,34 +5,13 @@ export DEBIAN_FRONTEND=noninteractive
 
 # shellcheck disable=SC2155
 
-function check_command() {
-    if ! command -v "$1" >/dev/null 2>&1; then
-        echo "Error: $1 not found"
-        exit 1
-    else
-        echo "$1 found"
-    fi
-}
-
 function install_pkg() {
-    _install_step1() {
-        local temp_dir
-        temp_dir=$(mktemp -d /tmp/pkg.step1.XXXXXX)
-        pushd "${temp_dir}" || exit 1
-        echo "pkg.step1."
-        popd || exit 1
-        rm -rf "${temp_dir}"
-    }
-
-    _install_step2() {
-        local temp_dir
-        temp_dir=$(mktemp -d /tmp/pkg.step2.XXXXXX)
-        pushd "${temp_dir}" || exit 1
-        echo "pkg.stdp2."
-        popd || exit 1
-        rm -rf "${temp_dir}"
-    }
-
-    _install_step1
-    _install_step2
+    local temp_dir
+    temp_dir=$(mktemp -d /tmp/pkg.step1.XXXXXX)
+    pushd "${temp_dir}" || exit 1
+    echo "install testcase pkg"
+    popd || exit 1
+    rm -rf "${temp_dir}"
 }
+
+install_pkg
--- dockerfiles/os/testcase/testcase.dockerfile
+++ dockerfiles/os/testcase/testcase.dockerfile
@@ -1,13 +1,16 @@
-FROM ubuntu:22.04
+ARG BASE_IMAGE=ubuntu:22.04
+FROM ${BASE_IMAGE}
 
 ENV DEBIAN_FRONTEND=noninteractive
+ENV PATH="/root/.cargo/bin:/root/.local/bin:$PATH"
 
-WORKDIR /lab
+SHELL ["/bin/bash", "-l", "-c"]
+WORKDIR /workspace
 
 COPY scripts .
 
 RUN set -ex \
     && find . -type f -name "*.sh" -exec chmod +x {} \;
 
-RUN set -ex \
-    && ./testcase.sh
+# RUN set -ex \
+#     && ./testcase.sh
