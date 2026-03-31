a2b15ebb28eb2b732508ac6e4f7845e9faf70e18
sqjian <shengqi.jian@gmail.com>
2026-03-31 06:18:32 +0000

    设置 git init 默认分支为 main

--- dockerfiles/os/ubuntu/scripts/internal/git/git.sh
+++ dockerfiles/os/ubuntu/scripts/internal/git/git.sh
@@ -3,5 +3,6 @@
 git config --global core.quotepath false
 git config --global core.autocrlf false
 git config --global core.safecrlf true
+git config --global init.defaultBranch main
 git config --global --get user.email >/dev/null || git config --global user.email shengqi.jian@gmail.com
 git config --global --get user.name >/dev/null || git config --global user.name sqjian
