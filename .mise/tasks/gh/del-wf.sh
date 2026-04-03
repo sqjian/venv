9dbb1592102f821024f8854aad12ee0a573ff4c5
sqjian <shengqi.jian@gmail.com>
2026-04-03 15:07:06 +0800

    1. fix .mise/config.toml 2. move task: cancel-wf to mise tasks

---
+++ .mise/tasks/gh/del-wf.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#MISE description="del all workflows."
+#MISE dir="{{config_root}}"
+
+export https_proxy=http://127.0.0.1:7890
+export http_proxy=http://127.0.0.1:7890
+export all_proxy=socks5://127.0.0.1:7890
+
+gh run list --limit 1000 --json databaseId -q '.[].databaseId' | xargs -I{} gh run delete {}
--- gh.mk
+++
@@ -1,4 +0,0 @@
-all: cancel-run-in-progress
-
-cancel-run-in-progress:
-	gh run ls -s in_progress --json databaseId -q .[].databaseId|xargs -n1 gh run cancel
