8cc0a8760f2c721b736ff749254daf5f683f79a9
sqjian <shengqi.jian@gmail.com>
2026-04-03 15:20:07 +0800

    Quote dockerfiles/app/crosstool/venv.sh

--- dockerfiles/app/crosstool/venv.sh
+++ dockerfiles/app/crosstool/venv.sh
@@ -3,7 +3,7 @@
 set -eux
 
 docker run --rm -it \
-	-v $(pwd)/generated/toolchain:/home/builder/build/toolchain \
-	-v $(pwd)/config/x86_64-gcc-8.5.0-glibc-2.28.config:/home/builder/build/.config \
+	-v "$(pwd)"/generated/toolchain:/home/builder/build/toolchain \
+	-v "$(pwd)"/config/x86_64-gcc-8.5.0-glibc-2.28.config:/home/builder/build/.config \
 	-w /home/builder/build \
 	sqjian/venv:crosstool bash
