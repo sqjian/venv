7dd4e227f397b054463903a59b51164287f6537c
sqjian <shengqi.jian@gmail.com>
2026-04-03 07:27:15 +0000

    add line number for .mise/tasks/gh/del-wf.sh

--- .mise/tasks/gh/del-wf.sh
+++ .mise/tasks/gh/del-wf.sh
@@ -6,4 +6,4 @@ export https_proxy=http://127.0.0.1:7890
 export http_proxy=http://127.0.0.1:7890
 export all_proxy=socks5://127.0.0.1:7890
 
-gh run list --limit 1000 --json databaseId -q '.[].databaseId' | xargs -I{} gh run delete {}
+gh run list --limit 1000 --json databaseId -q '.[].databaseId' | xargs -I{} gh run delete {} 2>&1 | nl
