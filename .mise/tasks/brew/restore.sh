f13e2f0475a28251db87e602d886f50124cee26b
sqjian <shengqi.jian@gmail.com>
2026-04-04 03:59:46 +0000

    添加 Homebrew 包管理的 mise 任务和 Brewfile 配置

---
+++ .mise/tasks/brew/restore.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#MISE description="restore packages installed by Homebrew package manager for Linux."
+#MISE dir="{{config_root}}/config"
+#MISE depends=["brew:pre"]
+#MISE depends_post = ["brew:post"]
+
+brew bundle install
