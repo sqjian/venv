3054dc20a3cd47a87fc105ce35cdc6b562ab6de6
sqjian <shengqi.jian@gmail.com>
2026-04-04 04:22:11 +0000

    delete mise.tasks.default

--- .mise/tasks/default.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#MISE description="default task: This task is executed when no specific task is provided. It can be used to set up the environment or perform any default actions."
-#MISE dir="{{config_root}}"
-
-echo "Executing default task..."
